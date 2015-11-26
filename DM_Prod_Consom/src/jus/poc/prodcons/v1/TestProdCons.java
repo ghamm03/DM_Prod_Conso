294a3ea84f6adef4581a1620714775da22cca381
ghamm <misa7403@hotmail.fr>
2015-11-26 15:27:50 +0100

    TestProdCons

---
+++ DM_Prod_Consom/src/jus/poc/prodcons/v1/TestProdCons.java
@@ -0,0 +1,19 @@
+package jus.poc.prodcons.v1;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+public class TestProdCons extends Simulateur {
+
+	public TestProdCons(Observateur observateur){
+		super(observateur);}
+
+	@Override
+	protected void run() throws Exception{
+	}
+
+	public static void main(String[] args){
+		new TestProdCons(new Observateur()).start();
+	}
+
+}
