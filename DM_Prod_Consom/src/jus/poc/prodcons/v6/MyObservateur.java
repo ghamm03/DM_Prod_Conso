54e670ccf9d0337ffc456176d71b2628a477503e
ghamm <misa7403@hotmail.fr>
2015-12-15 20:35:28 +0100

    Add commentaires

--- DM_Prod_Consom/src/jus/poc/prodcons/v6/MyObservateur.java
+++ DM_Prod_Consom/src/jus/poc/prodcons/v6/MyObservateur.java
@@ -15,8 +15,9 @@ public class MyObservateur{
 	private HashSet<_Producteur> prodList;
 	private HashSet<_Consommateur> consList;
 	private HashSet<Message> buffer;
-	private HashMap<_Consommateur, Message> msgWaitConso;
-	private HashMap<_Producteur, Message> msgWaitDepot;
+	private HashMap<_Consommateur, Message> msgWaitConso; //Msg retire en attente de consommation
+	private HashMap<_Producteur, Message> msgWaitDepot; //Msg produit en attente de depot
+
 
 	public void	init(int nbproducteurs, int nbconsommateurs, int nbBuffers)throws ControlException{
 		if(nbproducteurs<=0 || nbconsommateurs<=0 || nbBuffers <= 0)
@@ -36,6 +37,7 @@ public class MyObservateur{
 
 	public void newProducteur(_Producteur p)throws ControlException{
 		if(p!=null){
+			//Trop de producteur
 			if(this.prodList.size()+1>nbProd)
 				throw new ControlException(getClass(), "newProducteur");
 			prodList.add(p);
@@ -46,6 +48,7 @@ public class MyObservateur{
 
 	public void newConsommateur(_Consommateur c) throws ControlException{
 		if(c!=null){
+			//Trop de consommateur
 			if(this.consList.size()+1>nbConso)
 				throw new ControlException(getClass(), "newConsommateur");
 			consList.add(c);
@@ -57,12 +60,15 @@ public class MyObservateur{
 	public void retraitMessage(_Consommateur c, Message m) throws ControlException{
 		if(c==null || m==null)
 			throw new ControlException(getClass(), "retraitMessage");
+		//Si le message n'est pas le buffer
 		if(!buffer.remove(m))
 			throw new ControlException(getClass(), "retraitMessage");
 		msgWaitConso.put(c, m);
 	}
 
+	//Un message ne peut etre consomme que si il a ete enleve du buffer
 	public void consommationMessage(_Consommateur c, Message m, int tempsDeTraitement) throws ControlException{
+		//Si Valeurs invalides, consommateur n'est pas dans la liste, message encore dans le buffer
 		if(c==null || m==null || tempsDeTraitement<=0 || !consList.contains(c) || buffer.contains(m))
 			throw new ControlException(getClass(), "consommationMessage");
 		msgWaitConso.remove(c, m);
@@ -70,6 +76,7 @@ public class MyObservateur{
 	}
 
 	public void depotMessage(_Producteur p, Message m) throws ControlException{
+		//Si valeurs invalides, producteur n'est pas dans la liste
 		if(p==null || m==null || !prodList.contains(p))
 			throw new ControlException(getClass(), "depotMessage");
 		msgWaitDepot.remove(p, m);
@@ -77,6 +84,7 @@ public class MyObservateur{
 	}
 
 	public void productionMessage(_Producteur p, Message m, int tempsDeTraitement)throws ControlException{
+		//Si valeurs invalides, producteur n'est pas dans la liste
 		if(p==null || m==null || tempsDeTraitement<=0 || !prodList.contains(p))
 			throw new ControlException(getClass(), "consommationMessage");
 		msgWaitDepot.put(p, m);
